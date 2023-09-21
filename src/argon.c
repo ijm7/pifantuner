7b80531fafc59459bbb2e4a1e220d5b4c5ecc80d
Iain Morton <ijmorton500@gmail.com>
2023-09-21 15:54:11 +0100

    Run through webkit clang-format

--- src/argon.c
+++ src/argon.c
@@ -3,8 +3,8 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -15,50 +15,53 @@
 
 #include "pifantuner.h"
 
-static int32_t argon_fan_iface_update(const struct pifantuner_ctx *ctx, uint8_t value) {
-		assert(ctx);
-		assert(ctx->handle);
-		const struct pifantuner_argon *handle = ctx->handle;
-		return i2c_smbus_write_byte(handle->file, value);
+static int32_t argon_fan_iface_update(const struct pifantuner_ctx* ctx, uint8_t value)
+{
+    assert(ctx);
+    assert(ctx->handle);
+    const struct pifantuner_argon* handle = ctx->handle;
+    return i2c_smbus_write_byte(handle->file, value);
 }
 
-static void argon_fan_iface_destroy(struct pifantuner_ctx *ctx) {
-		assert(ctx);
-		struct pifantuner_argon *handle = ctx->handle;
-		if (handle) {
-				// reset fan
-				i2c_smbus_write_byte(handle->file, 0x00);
-				close(handle->file);
-				free(handle);
-				handle = NULL;
-		}
+static void argon_fan_iface_destroy(struct pifantuner_ctx* ctx)
+{
+    assert(ctx);
+    struct pifantuner_argon* handle = ctx->handle;
+    if (handle) {
+        // reset fan
+        i2c_smbus_write_byte(handle->file, 0x00);
+        close(handle->file);
+        free(handle);
+        handle = NULL;
+    }
 }
 
-static int argon_fan_iface_create(struct pifantuner_ctx *ctx) {
-		assert(ctx);
-		assert(!ctx->handle);
-		struct pifantuner_argon *handle = malloc(sizeof(struct pifantuner_argon));
-		if (!handle) {
-				goto error;
-		}
-		handle->file = open("/dev/i2c-1", O_RDWR);
-		if (handle->file < 0) {
-				goto error;
-		}
-		static const int addr = 0x1a;
-		if (ioctl(handle->file, I2C_SLAVE, addr) < 0) {
-				goto error;
-		}
+static int argon_fan_iface_create(struct pifantuner_ctx* ctx)
+{
+    assert(ctx);
+    assert(!ctx->handle);
+    struct pifantuner_argon* handle = malloc(sizeof(struct pifantuner_argon));
+    if (!handle) {
+        goto error;
+    }
+    handle->file = open("/dev/i2c-1", O_RDWR);
+    if (handle->file < 0) {
+        goto error;
+    }
+    static const int addr = 0x1a;
+    if (ioctl(handle->file, I2C_SLAVE, addr) < 0) {
+        goto error;
+    }
 
-		ctx->handle = handle;
-		return 0;
+    ctx->handle = handle;
+    return 0;
 error:
-		argon_fan_iface_destroy(ctx);
-		return 1;
+    argon_fan_iface_destroy(ctx);
+    return 1;
 }
 
 struct pifantuner_iface argon_fan_iface = {
-		.create = argon_fan_iface_create,
-		.update = argon_fan_iface_update,
-		.destroy = argon_fan_iface_destroy
+    .create = argon_fan_iface_create,
+    .update = argon_fan_iface_update,
+    .destroy = argon_fan_iface_destroy
 };
