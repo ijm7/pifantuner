8903857998141e686e8474219b747b69f871e411
Iain Morton <ijmorton500@gmail.com>
2023-10-01 11:56:40 +0100

    Update style

--- src/main.c
+++ src/main.c
@@ -8,25 +8,23 @@ static const unsigned int poll_time_seconds = 1;
 
 static bool run = true;
 
-static void signal_handler(int) {
-  run = false;
+static void signal_handler(int) { run = false; }
+
+static void run_daemon(const struct pifantuner_ctx *ctx) {
+        while (run) {
+                pifantuner_poll(ctx);
+                sleep(poll_time_seconds);
+        }
 }
 
-static void run_daemon(const struct pifantuner_ctx* ctx) {
-  while (run) {
-    pifantuner_poll(ctx);
-    sleep(poll_time_seconds);
-  }
-}
+int main(int argc, char **argv) {
+        signal(SIGINT, signal_handler);
+        struct pifantuner_ctx *ctx = pifantuner_create();
+        if (!ctx) {
+                return 1;
+        }
+        run_daemon(ctx);
 
-int main(int argc, char** argv) {
-  signal(SIGINT, signal_handler);
-  struct pifantuner_ctx* ctx = pifantuner_create();
-  if (!ctx) {
-    return 1;
-  }
-  run_daemon(ctx);
-
-  pifantuner_destroy(ctx);
-  return 0;
+        pifantuner_destroy(ctx);
+        return 0;
 }
